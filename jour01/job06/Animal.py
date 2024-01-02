a06a7bf7b4734685d6787fa5d8540630b65020c8
marion-borne <marion.borne@laplateforme.io>
2024-01-02 14:39:10 +0100

    ajout du job06

---
+++ jour01/job06/Animal.py
@@ -0,0 +1,27 @@
+class Animal:
+    
+    def __init__(self):
+        self.age = 0
+        self.prenom = "" 
+        
+    def set_x(self, age):
+        self.age = age
+        
+    def set_y(self, prenom):
+        self.prenom = prenom
+        
+    def vieillir(self):
+        print(f"L'âge de l'animal {self.age} ans")
+        self.age += 1
+        print(f"L'âge de l'animal {self.age} ans")
+        
+    def nommer(self, prenom):
+        self.prenom = prenom
+        print(f"L'animal se nomme {self.prenom}")
+        
+        
+caracteristique = Animal()
+caracteristique.vieillir()
+caracteristique.nommer("Luna")
+
+
